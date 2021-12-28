18cc9396f944845929407d762767fa9bddd4931b
evoniuk <jack.evoniuk@gmail.com>
2021-12-27 23:58:00 -0800

    Add support for all basic formatting
    Add support for bold, dim, underlined, blinking, inverted, and
    hidden text.

--- render.c
+++ render.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 char* file_contents(char* filename);
 char* substitute_escapes(char* text);
@@ -37,28 +38,71 @@ char* file_contents(char* filename)
 
 char* substitute_escapes(char* text)
 {
-    // substitutions acts as an associative array between ASCII chars
-    // and their equivalent escape sequences in terminal markdown
-    char* substitutions[128] = {0};
-    substitutions['>'] = "\e[7m";   // begin inverted
-    substitutions['<'] = "\e[27m";  // end inverted
+    // status tells whether text is currently in special state
+    bool status[128] = {0};
+
+    char* substitutions_begin[128] = {0};
+    char* substitutions_end[128]   = {0};
+
+    substitutions_begin['*'] = "\e[1m";   // bold
+    substitutions_end['*']   = "\e[22m";
+    substitutions_begin['~'] = "\e[2m";   // dim
+    substitutions_end['~']   = "\e[22m";
+    substitutions_begin['_'] = "\e[4m";   // underline
+    substitutions_end['_']   = "\e[24m";
+    substitutions_begin['@'] = "\e[5m";   // blink
+    substitutions_end['@']   = "\e[25m";
+    substitutions_begin['$'] = "\e[7m";   // inverted
+    substitutions_end['$']   = "\e[27m";
+    substitutions_begin['`'] = "\e[8m";   // hidden
+    substitutions_end['`']   = "\e[28m";
 
     int result_len = 0;
-    for (int i = 0; i < 128; i++)
-        if (substitutions[i]) 
-            result_len += strlen(substitutions[i]);
+    for (char* text_i = text; *text_i; text_i++)
+    {
+        if (*text_i == '\\') // skip past escaped chars
+        {
+            text_i += 2;
+            result_len++;
+        }
+
+        if (substitutions_begin[*text_i])
+        {
+            status[*text_i] = !status[*text_i];
+            result_len += strlen(
+                status[*text_i] ?
+                    substitutions_begin[*text_i] :
+                    substitutions_end[*text_i]
+            );
+        }
+
+        else result_len++;
+    }
+
+    memset(status, 0, sizeof status); // reset status array
 
     char* result = calloc(result_len + 1, sizeof(char));
 
-    for (char* result_index = result; *text; text++)
+    for (char* result_i = result; *text; text++)
     {
-        char* substitution = substitutions[*text];
+        if (*text == '\\') // provide '\' as an escape character
+        {
+            *result_i++ = *++text;
+            continue;
+        }
+
+        if (substitutions_begin[*text]) // if char is special char, change status
+            status[*text] = !status[*text];
+
+        char* substitution = status[*text] ?
+            substitutions_begin[*text] :
+            substitutions_end[*text];
 
         if (substitution)
             for (int i = 0; i < strlen(substitution); i++)
-                *result_index++ = substitution[i];
+                *result_i++ = substitution[i];
 
-        else *result_index++ = *text;
+        else *result_i++ = *text;
     }
 
     return result;
